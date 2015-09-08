343ad916c8aaddb26839669d26e71f8d3dfe7ed5
sriharshajujjavarapu <sri.cs.nitw@gmail.com>
2015-09-08 14:26:36 +0530

    bug fixes and UI enhancements

--- app/helpers/book_helper.rb
+++ app/helpers/book_helper.rb
@@ -6,4 +6,11 @@ module BookHelper
       "btn btn-primary disabled"
     end
   end
+  def validate_user
+    if current_user!=@book.user && @book.requests.find_by(user_id:current_user,status:Request::PENDING_CODE) == nil
+      true
+    else
+      false
+    end
+  end
 end
