c27e66e671313eb0fdb26c57e3364259f5b929a5
sriharshajujjavarapu <sri.cs.nitw@gmail.com>
2015-09-24 11:33:50 +0530

    changed notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,10 +2,14 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
   def index
     @notifications=current_user.unread_notifications
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
   def read
     @notification = Notification.find(params[:id])
     @notification.read
-    redirect_to user_notifications_path
+    redirect_to request.referer
   end
 end
